78400a5e568ef50ebdbde9cf79097a675bc3fae1
дмитрий строчук <yda4ya@dmitrijs-MacBook-Pro.local>
2026-04-10 01:04:18 +0300

    Lab 2: создана архитектура сервера (src/, public/, server.js)

---
+++ lab2/src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json()); //понимает JSON от клиента
+app.use(express.static('public')); //открывает файлы из папки public
+
+//тестовый маршрут
+app.get('/', (req, res) => {
+    res.send('<h1>SwimTrack Lab 2 запущен</h1>');
+});
+
+//запуск сервера
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
